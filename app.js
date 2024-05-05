55b5d3382f6ee799f92d710f76bfbc1b3f5e858a
Darell Mona <s2200064@student.unklab.ac.id>
2024-05-05 12:00:38 +0800

    update: - api login - api create new admin

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const adminController = require("./src/admin/admin.controller");
 
 const app = express();
+app.use(express.json());
+
+app.use("/admin", adminController);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
