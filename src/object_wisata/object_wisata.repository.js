2eaf21db05b37fb072b99f2473d65057623d9c33
Darell Mona <s2200064@student.unklab.ac.id>
2024-05-05 20:55:03 +0800

    update: - add find all destination endpoint - add find destination by id endpoint - add update destination data endpoint

--- src/object_wisata/object_wisata.repository.js
+++ src/object_wisata/object_wisata.repository.js
@@ -14,6 +14,43 @@ const createDestination = async (payload) => {
   });
 };
 
+const findAllDestination = async () => {
+  return await prisma.objek_wisata.findMany({
+    include: {
+      gambar: true,
+    },
+  });
+};
+
+const findDestinationById = async (id) => {
+  return await prisma.objek_wisata.findUnique({
+    where: {
+      id,
+    },
+    include: {
+      gambar: true,
+    },
+  });
+};
+
+const updateDestination = async (id, payload) => {
+  const { nama, alamat, keterangan, waktu_operasional } = payload;
+  return await prisma.objek_wisata.update({
+    where: {
+      id,
+    },
+    data: {
+      nama,
+      alamat,
+      keterangan,
+      waktu_operasional,
+    },
+  });
+};
+
 module.exports = {
   createDestination,
+  findAllDestination,
+  findDestinationById,
+  updateDestination,
 };
--- src/object_wisata/object_wisata.service.js
+++ src/object_wisata/object_wisata.service.js
@@ -1,4 +1,9 @@
-const { createDestination } = require("./object_wisata.repository");
+const {
+  createDestination,
+  findAllDestination,
+  findDestinationById,
+  updateDestination,
+} = require("./object_wisata.repository");
 
 const createNewDestination = async (payload) => {
   const { images, nama, alamat, keterangan, waktu_operasional } = payload;
@@ -16,6 +21,38 @@ const createNewDestination = async (payload) => {
   return await createDestination(payload);
 };
 
+const getAllDestination = async () => {
+  const data = await findAllDestination();
+  const normalize = data.map((item) => ({
+    ...item,
+    gambar: item.gambar.map(
+      (img) => "http://localhost:3000/" + img.path.replace(/\\/g, "/")
+    ),
+  }));
+  return normalize;
+};
+
+const getDestinationById = async (id) => {
+  const data = await findDestinationById(id);
+  if (!data) {
+    throw new Error("Destination not found");
+  }
+  const normalize = {
+    ...data,
+    gambar: data.gambar.map(
+      (img) => "http://localhost:3000/" + img.path.replace(/\\/g, "/")
+    ),
+  };
+  return normalize;
+};
+
+const updateDestinationData = async (id, payload) => {
+  return await updateDestination(id, payload);
+};
+
 module.exports = {
   createNewDestination,
+  getAllDestination,
+  getDestinationById,
+  updateDestinationData,
 };
