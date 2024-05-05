6ccdf0ea59398e5b53d8b6f23d6e9c1ff0813084
Darell Mona <s2200064@student.unklab.ac.id>
2024-05-05 12:43:57 +0800

    update: - create api change admin password - create api delete admin account

--- src/admin/admin.repository.js
+++ src/admin/admin.repository.js
@@ -8,13 +8,37 @@ const getAdminByEmail = async (email) => {
   });
 };
 
+const getAdminById = async (id) => {
+  return await prisma.admin.findUnique({
+    where: {
+      id,
+    },
+  });
+};
+
 const createAdmin = async (payload) => {
   return await prisma.admin.create({
     data: payload,
   });
 };
 
+const updateAdmin = async (id, payload) => {
+  return await prisma.admin.update({
+    where: { id },
+    data: payload,
+  });
+};
+
+const deleteAdmin = async (id) => {
+  return await prisma.admin.delete({
+    where: { id },
+  });
+};
+
 module.exports = {
   getAdminByEmail,
+  getAdminById,
   createAdmin,
+  updateAdmin,
+  deleteAdmin,
 };
--- src/admin/admin.service.js
+++ src/admin/admin.service.js
@@ -1,4 +1,10 @@
-const { getAdminByEmail, createAdmin } = require("./admin.repository");
+const {
+  getAdminByEmail,
+  createAdmin,
+  getAdminById,
+  updateAdmin,
+  deleteAdmin,
+} = require("./admin.repository");
 
 const login = async (payload) => {
   const { email, password } = payload;
@@ -19,7 +25,28 @@ const createNewAdmin = async (payload) => {
   return await createAdmin(payload);
 };
 
+const changePassword = async (id, payload) => {
+  const { oldPassword, newPassword } = payload;
+  const admin = await getAdminById(id);
+  if (!admin) {
+    throw new Error("Admin not found");
+  } else if (admin.password !== oldPassword) {
+    throw new Error("Invalid password");
+  }
+  return await updateAdmin(id, { password: newPassword });
+};
+
+const deleteAccount = async (id) => {
+  const admin = await getAdminById(id);
+  if (!admin) {
+    throw new Error("Admin not found");
+  }
+  return await deleteAdmin(id);
+};
+
 module.exports = {
   login,
   createNewAdmin,
+  changePassword,
+  deleteAccount,
 };
